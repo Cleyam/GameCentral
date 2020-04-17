93b15fec60adb45e2fafca71d926271989b924bc
Cleyam <cleyam@gmail.com>
2020-04-17 11:36:16 +0200

    csrf

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('logout-button', require('./components/LogoutButton.vue').default);
 Vue.component('search-button', require('./components/SearchButton.vue').default);
-
 const app = new Vue({
     el: '#app'
 });
--- resources/js/components/LogoutButton.vue
+++ resources/js/components/LogoutButton.vue
@@ -1,5 +1,6 @@
 <template>
   <form id="logout-form" :action="route" method="POST">
+    <input type="hidden" name="_token" :value="csrf" />
     <button type="submit" class="btn logout bg-orange shadow-sm">
       <i class="fas fa-sign-out-alt fs-30 text-light"></i>
     </button>
@@ -8,6 +9,11 @@
 
 <script>
 export default {
-  props: ["route"]
+  props: ["route"],
+  data: function() {
+    return {
+      csrf: document.head.querySelector('meta[name="csrf-token"]').content
+    };
+  }
 };
 </script>
